e5ce8ca6a913210b272f217d9093630d6faa4828
Ashwin746544 <ahpatel99999@gmail.com>
2022-03-25 16:49:34 -0700

    perf: add React.memo and useCallback() and fetching images is done in searchbar component

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,8 +1,42 @@
 import './Searchbar.css';
+import React, { useState, useEffect } from 'react';
+import { createApi } from 'unsplash-js';
+
+
+const unsplash = createApi({
+  accessKey: "q57dhCGtqh0GwXvKvRdcCQas8KXlTHMKmPfrm_0ehkI"
+});
+
 const Searchbar = (props) => {
+
+  const [searchedText, setSearchedText] = useState('');
+  console.log("SearchBar Rendered!");
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      // setIsLoading(true);
+      props.storeLoadingState(true);
+      console.log('fetching images...  ' + searchedText);
+      unsplash.search.getPhotos({ query: searchedText == '' ? 'user' : searchedText, page: 1, perPage: 15 })
+        .then(res => {
+          console.log(res);
+          // setFetchedImages(res.response.results);
+          // setIsLoading(false);
+          props.storeFetchedImages(res.response.results);
+          props.storeLoadingState(false);
+        }
+        );
+    }, searchedText == '' ? 0 : 500);
+    return () => { clearTimeout(id) };
+  }, [searchedText]);
+
+  const updateSearchTextHandler = (text) => {
+    setSearchedText(text);
+  }
+
   return <div className="Searchbar">
-    <input type="text" name="name" id="name" placeholder="search..." onChange={(event) => props.textChanged(event.target.value)} />
+    <input type="text" name="name" id="name" placeholder="search..." onChange={(event) => updateSearchTextHandler(event.target.value)} />
   </div>
 }
 
-export default Searchbar;
+export default React.memo(Searchbar);
